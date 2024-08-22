02347e3a4a0135d356c876125d5109afcfa88b5c
9Quake90 <reinardpiet@gmail.com>
2024-08-22 11:22:47 +0200

    The first stuff

---
+++ PRG281_Project/PRG281_Project/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PRG281_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
