9be6ffd8b4e954309a83fd3ce214225d78804ca7
Caitlin Burnett <chburnett02@gmail.com>
2024-08-24 14:57:21 +0200

    Fixed income class

--- PRG281_Project/PRG281_Project/income.cs
+++ PRG281_Project/PRG281_Project/income.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PRG281_Project
 {
-    internal class Income:FinancialEntity,
+    internal class Income:FinancialEntity, iFinancialOperation
     {
+        public Income(string name, double amount) : base(name, amount) { }
+
+        public void Execute()
+        {
+        }
+
+        public override void Display()
+        {
+        }
     }
+}
 }
