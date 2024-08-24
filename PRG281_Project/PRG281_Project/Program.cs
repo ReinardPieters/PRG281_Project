efcd77d145c26c7fee817732b65824e00e5317df
9Quake90 <reinardpiet@gmail.com>
2024-08-24 14:32:53 +0200

    Added the Menu

--- PRG281_Project/PRG281_Project/Program.cs
+++ PRG281_Project/PRG281_Project/Program.cs
@@ -1,11 +1,111 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 namespace PRG281_Project
 {
+    public enum MenuOptions
+    {
+        [Description("Add Income")]
+        AddIncome = 1,
+
+        [Description("Add Expense")]
+        AddExpense,
+
+        [Description("Set Savings")]
+        SetSavings,
+
+        [Description("Display Summary")]
+        DisplaySummary,
+
+        [Description("Start Monitoring Expenses")]
+        StartMonitoringExpenses,
+
+        [Description("Exit")]
+        Exit
+    }
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello World");
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("Menu:");
+                foreach (MenuOptions option in Enum.GetValues(typeof(MenuOptions)))
+                {
+                    Console.WriteLine($"{(int)option}. {GetEnumDescription(option)}");
+                }
+
+                Console.Write("Select an option: ");
+                try
+                {
+                    int selectedOption = int.Parse(Console.ReadLine());
+
+                    if (Enum.IsDefined(typeof(MenuOptions), selectedOption))
+                    {
+                        MenuOptions choice = (MenuOptions)selectedOption;
+                        switch (choice)
+                        {
+                            case MenuOptions.AddIncome:
+                                Console.WriteLine("You chose to Add Income.");
+                                break;
+                            case MenuOptions.AddExpense:
+                                Console.WriteLine("You chose to Add Expense.");
+                                break;
+                            case MenuOptions.SetSavings:
+                                Console.WriteLine("You chose to Add Savings.");
+                                break;
+                            case MenuOptions.DisplaySummary:
+                                Console.WriteLine("You chose to Display Summary.");
+                                break;
+                            case MenuOptions.StartMonitoringExpenses:
+                                Console.WriteLine("You chose to Start Monitoring Expenses.");
+                                break;
+                            case MenuOptions.Exit:
+                                Console.WriteLine("Exiting...");
+                                running = false;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid option. Please choose a valid menu option.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                }
+
+                Console.WriteLine();
+            }
+        }
+        public static string GetEnumDescription(Enum value)
+        {
+            try
+            {
+                FieldInfo fi = value.GetType().GetField(value.ToString());
+                DescriptionAttribute[] attributes = (DescriptionAttribute[])fi.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attributes != null && attributes.Length > 0)
+                {
+                    return attributes[0].Description;
+                }
+                else
+                {
+                    return value.ToString().Replace('_', ' ');
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while retrieving the description: " + ex.Message);
+                return "Unknown"; // Return a default value in case of an error
+            }
         }
     }
+
 }
--- PRG281_Project/PRG281_Project/obj/Debug/net6.0/PRG281_Project.AssemblyInfo.cs
+++ PRG281_Project/PRG281_Project/obj/Debug/net6.0/PRG281_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRG281_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e3aa040fba42b34911a3e90fc55ff655c0aee61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60ef6c243135cbf71cc4609a2614548096066585")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRG281_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRG281_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
