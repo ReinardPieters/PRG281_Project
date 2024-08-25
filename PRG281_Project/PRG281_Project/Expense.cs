80f615f41ac45751dd5e50e1fa60467f40688ad8
Caitlin Burnett <chburnett02@gmail.com>
2024-08-25 13:01:58 +0200

    Expense class

--- PRG281_Project/PRG281_Project/Expense.cs
+++ PRG281_Project/PRG281_Project/Expense.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace PRG281_Project
 {
-    internal class Expense
+    internal class Expense: FinancialEntity, iFinancialOperation
     {
-    }
+
+        public Expense(string name, double amount) : base(name, amount) { }
+
+        public void Execute()
+        {
+           
+        }
+
+        protected virtual void OnOverspendingAlert(EventArgs e)
+        {
+        }
+
+        public override void Display()
+        {
+        }
+    
+
+}
 }
